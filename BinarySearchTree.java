39ad52f173d0ab3623103e3f19a451de55e5face
Mark Baltes <baltesm@coyote.csusb.edu>
2016-05-14 09:32:41 -0700

    Added delete method to binary search tree.

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -45,13 +45,89 @@ public class BinarySearchTree {
     }
   }
   
-  public void delete(int key) {
-    //
+  public boolean delete(int key) {
+    Node current = root;
+    Node parent = root;
+    boolean isLeftChild = true;
+    while (current.getData() != key) { // Search for node
+      parent = current;
+      if (key < current.getData()) { // Go left.
+        isLeftChild = true;
+        current = current.getLeftChild();
+      } else { // Go right.
+        isLeftChild = false;
+        current = current.getRightChild();
+      }
+      if (current == null) { // Didn't find, so return false.
+        return false;
+      }
+    }
+    // If here, we have found the node to delete.
+    // First case: Node has no children, so we simply delete it.
+    if (current.getLeftChild() == null && current.getRightChild() == null) {
+      if (current == root) {
+        root = null;
+      } else if (isLeftChild) {
+        parent.setLeftChild(null);
+      } else {
+        parent.setRightChild(null);
+      }
+    } else if (current.getRightChild() == null) {
+      // Second case: Node has no right child, so we replace with left
+      // subtree.
+      if (current == root) {
+        root = current.getLeftChild();
+      } else if (isLeftChild) {
+        parent.setLeftChild(current.getLeftChild());
+      } else {
+        parent.setRightChild(current.getLeftChild());
+      }
+    } else if (current.getLeftChild() == null) {
+      // Third case: Node has no left child, so replace with right subtree.
+      if (current == root) {
+        root = current.getRightChild();
+      } else if (isLeftChild) {
+        parent.setLeftChild(current.getRightChild());
+      } else {
+        parent.setRightChild(current.getRightChild());
+      }
+    } else { // Node has two children, so replace with inorder successor.
+      // We first find the successor of node to delete.
+      Node successor = getSuccessor(current);
+      // Connect parent of current node to successor node.
+      if (current == root) {
+        root = successor;
+      } else if (isLeftChild) {
+        parent.setLeftChild(successor);
+      } else {
+        parent.setRightChild(successor);
+      }
+      // Connect successor to current node's left child.
+      successor.setLeftChild(current.getLeftChild());
+    }
+    // Successor can't have a left child.
+    return true;
   }
   
+  private Node getSuccessor(Node delNode) {
+    Node successorParent = delNode;
+    Node successor = delNode;
+    Node current = delNode.getRightChild();
+    while (current != null) {
+      successorParent = successor;
+      successor = current;
+      current = current.getLeftChild();
+    }
+    if (successor != delNode.getRightChild()) {
+      successorParent.setLeftChild(successor.getRightChild());
+      successor.setRightChild(delNode.getRightChild());
+    }
+    return successor;
+  }
+
   public void preOrderTraversal(Node root) {
     if (root != null) {
-      System.out.println(root.getData() + " ");
+      System.out.print(root.getData() + " ");
       preOrderTraversal(root.getLeftChild());
       preOrderTraversal(root.getRightChild());
     }
@@ -59,17 +135,17 @@ public class BinarySearchTree {
   
   public void inOrderTraversal(Node root) {
     if (root != null) {
-      preOrderTraversal(root.getLeftChild());
-      System.out.println(root.getData() + " ");
-      preOrderTraversal(root.getRightChild());
+      inOrderTraversal(root.getLeftChild());
+      System.out.print(root.getData() + " ");
+      inOrderTraversal(root.getRightChild());
     }
   }
   
   public void postOrderTraversal(Node root) {
     if (root != null) {
-      preOrderTraversal(root.getLeftChild());
-      preOrderTraversal(root.getRightChild());
-      System.out.println(root.getData() + " ");
+      postOrderTraversal(root.getLeftChild());
+      postOrderTraversal(root.getRightChild());
+      System.out.print(root.getData() + " ");
     }
   }
   
@@ -78,6 +154,9 @@ public class BinarySearchTree {
     b.insert(32);
     b.insert(27);
     b.insert(57);
+    b.insert(5);
+    b.insert(40);
+    b.delete(32);
     
     b.preOrderTraversal(b.root);
     System.out.println();
@@ -85,11 +164,6 @@ public class BinarySearchTree {
     System.out.println();
     b.postOrderTraversal(b.root);
     
-    Node found = b.find(587);
-    if (found != null) {
-      System.out.println("Found.");
-    } else {
-      System.out.println("Not found.");
-    }
+    
   }
 }
